0506752ede0e44d4c8ef1826f2cbee6c96778d7f
Martin M Syvertsen <martin.syvertsen@gmail.com>
2013-11-15 22:43:46 +0100

    Pretties

--- src/no/demo/DemoMain.java
+++ src/no/demo/DemoMain.java
@@ -12,10 +12,41 @@ public class DemoMain extends PApplet {
         size(1280, 1024);
         background(0);
 
-        butterflyArrayList.add(b1);
-        butterflyArrayList.add(b2);
+        for(int i = 0; i< 10; i++){
+            butterflyArrayList.add(GenerateRandomButterfly());
+        }
         
         analyser = new MuzakAnalyser(this);
+    }
+
+    private Butterfly GenerateRandomButterfly(){
+        int Min = 0;
+        int Max = 250;
+
+        int random1 = Min + (int)(Math.random() * ((Max - Min) + 1));
+        int random2 = Min + (int)(Math.random() * ((Max - Min) + 1));
+        int random3 = Min + (int)(Math.random() * ((Max - Min) + 1));
+
+        int random4 = Min + (int)(Math.random() * ((Max - Min) + 1));
+        int random5 = Min + (int)(Math.random() * ((Max - Min) + 1));
+        int random6 = Min + (int)(Math.random() * ((Max - Min) + 1));
+
+        Color randColor1 = new Color(random1,random2,random3);
+        Color randColor2 = new Color(random4,random5,random6);
+
+        int PosMax = 2000;
+
+        int pos = Min + (int)(Math.random() * ((PosMax - Min) + 1));
+
+        int SpeedMax = 10;
+
+        int position = Min + (int)(Math.random() * ((PosMax - Min) + 1));
+
+        int speed =  1 + (int)(Math.random() * ((SpeedMax - 1) + 1));
+
+        Butterfly b1 = new Butterfly(randColor1, randColor2, position, 0, speed);
+
+        return b1;
     }
 
     int x = 0;
@@ -32,12 +63,6 @@ public class DemoMain extends PApplet {
 
     ArrayList<Butterfly> butterflyArrayList = new ArrayList<Butterfly>();
 
-    Color red = new Color(100,0,0);
-    Color blue = new Color(0,0,100);
-
-    Butterfly b1 = new Butterfly(red,blue,300,0,1);
-    Butterfly b2 = new Butterfly(red,blue,600,0,3);
-
     public class Butterfly {
         private Color bodyColor, wingColor;
         private int xOffset, yPosition;
@@ -53,6 +78,21 @@ public class DemoMain extends PApplet {
         }
 
         public void move() {
+            if(yPosition > 2000){
+                int Min = 0;
+                int PosMax = 2000;
+
+                int pos = Min + (int)(Math.random() * ((PosMax - Min) + 1));
+
+                int SpeedMax = 10;
+
+                int position = Min + (int)(Math.random() * ((PosMax - Min) + 1));
+
+                int speed =  1 + (int)(Math.random() * ((SpeedMax - 1) + 1));
+                xOffset = position;
+                this.speed = speed;
+                yPosition = -300;
+            }
             yPosition = yPosition + speed;
         }
 
@@ -109,29 +149,6 @@ public class DemoMain extends PApplet {
 
     }
 
-    public void FlockOfButterflies() {
-        Color bodyColor = new Color(140, 50, 7);
-        Color wingColor = new Color(100, 0, 0);
-
-        DrawButterflyMethod(bodyColor, wingColor, 600, butterflyPosition);
-
-        bodyColor = new Color(78, 170, 90);
-        wingColor = new Color(5, 110, 0);
-
-        DrawButterflyMethod(bodyColor, wingColor, 400, butterflyPosition + 200);
-
-
-        bodyColor = new Color(78, 50, 90);
-        wingColor = new Color(5, 5, 230);
-
-        DrawButterflyMethod(bodyColor, wingColor, 700, butterflyPosition - 700);
-
-        bodyColor = new Color(98, 150, 90);
-        wingColor = new Color(45, 10, 0);
-
-        DrawButterflyMethod(bodyColor, wingColor, 1000, butterflyPosition - 300);
-    }
-
     public class Color {
         public int R, G, B;
 
@@ -172,54 +189,5 @@ public class DemoMain extends PApplet {
                 xOffset + 400 - x, yOffset + 400 - x,
                 xOffset + 100, yOffset + 300);
 
-    }
-
-    private void Butterfly() {
-        //Butterfly body
-        stroke(50, 50, 100);
-        fill(80, 150, 50);
-        quad(
-                600, 200,
-                700, 200,   //Punkg - 2
-                700, 500,   //Punkt - 3
-                600, 500);
-
-
-        //Wing color
-        stroke(color, 50, 100);
-        fill(67, 3, 51);
-
-        //Wing One
-        quad(
-                300 + x, 100 + y,
-                600, 200,
-                600, 500,
-                300 + x, 600 - y);
-
-
-        //Wing Two
-        quad(
-                700, 200,
-                1000 - x, 100 + x,
-                1000 - x, 600 - x,
-                700, 500);
-
-
-        if (x > 200) {
-            turn = true;
-        }
-
-        if (x < 1) {
-            turn = false;
-        }
-
-
-        if (turn) {
-            x -= speed;
-            y -= speed;
-        } else {
-            x += speed;
-            y += speed;
-        }
     }
 }
