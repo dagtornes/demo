d1021474a7d460397eee2ea309ec01b9347119e2
Dag Øyvind Tornes <dagtornes@gmail.com>
2013-11-15 22:44:06 +0100

    Muzak has spectrum draw for debug

--- src/no/demo/MuzakAnalyser.java
+++ src/no/demo/MuzakAnalyser.java
@@ -4,13 +4,16 @@ import processing.core.PApplet;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
 import ddf.minim.analysis.BeatDetect;
+import ddf.minim.analysis.FFT;
 
 public class MuzakAnalyser {
 	PApplet parent;
 	Minim minim;
 	AudioPlayer song;
+
 	BeatDetect bd;
-	
+	FFT fft;
+
 	public MuzakAnalyser(PApplet parent) {
 		this.parent = parent;
 		minim = new Minim(parent);
@@ -18,16 +21,34 @@ public class MuzakAnalyser {
 		bd = new BeatDetect(song.bufferSize(), song.sampleRate());
 		bd.detectMode(BeatDetect.FREQ_ENERGY);
 		song.play();
+
+		fft = new FFT(song.bufferSize(), song.sampleRate());
 	}
-	
+
 	private int count = 0;
+
 	public float getBeat() {
 		bd.detect(song.mix);
 		if (bd.isKick()) {
 			count = 60;
 		}
 		count--;
-		
+
 		return Math.max(count / 60.0f, 0.0f);
+	}
+
+	public void drawSpectrum() {
+		fft.forward(song.mix);
+
+		parent.pushMatrix();
+		
+		parent.translate(0.0f, (float) parent.height);
+		parent.scale(3.0f);
+		parent.stroke(255, 0, 0, 128);
+		for (int i = 0; i < fft.specSize(); i++) {
+			parent.line(i, 0, i, -fft.getBand(i) * 4);
+		}
+		
+		parent.popMatrix();
 	}
 }
