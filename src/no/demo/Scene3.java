093a5d9598425ad3edd456a1f851b952b0abe452
Martin M Syvertsen <martin.syvertsen@gmail.com>
2013-11-16 15:30:19 +0100

    Working on Scene 3

---
+++ src/no/demo/Scene3.java
@@ -0,0 +1,31 @@
+package no.demo;
+
+import processing.core.PApplet;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: nomarsyv
+ * Date: 16.11.13
+ * Time: 14:20
+ * To change this template use File | Settings | File Templates.
+ */
+public class Scene3 implements Scene {
+
+    private long duration;
+    private long start;
+    private SpectrumBox spectrumBox;
+
+    public Scene3(PApplet parent, long duration){
+
+    }
+
+    @Override
+    public void draw(PApplet render) {
+
+    }
+
+    @Override
+    public boolean done() {
+        return false;
+    }
+}
