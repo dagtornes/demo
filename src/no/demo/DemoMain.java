bad54a9e3771cbbfcc9d0fce435b4027bdd58860
Dag Øyvind Tornes <dagtornes@gmail.com>
2013-11-16 14:33:54 +0100

    Butterflies is a separate scene

--- src/no/demo/DemoMain.java
+++ src/no/demo/DemoMain.java
@@ -18,17 +18,11 @@ public class DemoMain extends PApplet {
     public void setup() {
     	
     	scenes = new ArrayList<Scene>();
-    	scenes.add(new Scene1(this, 10 * 1000));
+    	scenes.add(new SceneButterflies(this));
     	
         size(1280, 1024, P2D);
         background(0);
-
-        for (int i = 0; i < 10; i++) {
-            butterflyArrayList.add(GenerateRandomButterfly());
-        }
-
         for (int i = 0; i < numSpectrumBoxes; i++) {
-            Color color = new Color(100, 1, 1);
             spectrumBoxArrayList.add(GenerateRandomSpectrumBox(i));
         }
 
@@ -48,36 +42,6 @@ public class DemoMain extends PApplet {
         return new SpectrumBox(randColor1,position,0);
     }
 
-    private Butterfly GenerateRandomButterfly() {
-        int Min = 0;
-        int Max = 250;
-
-        Color randColor1 = Color.random(Min, Max);
-        Color randColor2 = Color.random(Min, Max);
-
-        int PosMax = 2000;
-
-
-        int pos = Min + (int) (Math.random() * ((PosMax - Min) + 1));
-
-        int SpeedMax = 10;
-
-        int position = Min + (int) (Math.random() * ((PosMax - Min) + 1));
-
-        int speed = 1 + (int) (Math.random() * ((SpeedMax - 1) + 1));
-
-        Butterfly b1 = new Butterfly(this, randColor1, randColor2, position, 0, speed);
-
-        return b1;
-    }
-
-    int x = 0;
-    int y = 0;
-    int x2 = 0;
-    int y2 = 0;
-    int color = 0;
-    boolean turn = true;
-    int speed = 5;
 
     ArrayList<Butterfly> butterflyArrayList = new ArrayList<Butterfly>();
     ArrayList<SpectrumBox> spectrumBoxArrayList = new ArrayList<SpectrumBox>();
@@ -124,54 +88,15 @@ public class DemoMain extends PApplet {
 
     public void draw() {
         clear();
-
         
         if (scenes.size() > 0) {
-        	//System.out.println("DRAW!");
         	Scene current = scenes.get(0);
         	current.draw(this);
         	if (current.done()) {
         		scenes.remove(0);
         	}
         }
-//
-//        particles.draw(this);
-//        
-//        for (SpectrumBox spectrumBox : spectrumBoxArrayList) {
-//        	spectrumBox.draw();
-//        }
-//        
-//        for(Butterfly butterfly : butterflyArrayList){
-//            butterfly.move();
-//            butterfly.draw(x, y);
-//        }
-//
-//        if (turn) {
-//            x -= speed;
-//            y -= speed;
-//        } else {
-//            x += speed;
-//            y += speed;
-//        }
-//
-//        if (x > 200) {
-//            turn = true;
-//        }
-//
-//        if (x < 1) {
-//            turn = false;
-//        }
-//
-//
-//        float beat = analyser.getBeat();
-//        if (beat > 0.0) {
-//        	fill(255, 0, 0);
-//        	stroke(255, 0, 0);
-//        	Render.circle(this, 20.0f, 20.0f, 18.0f * beat, 32);
-//        }
-
     }
-
 
     private void DrawMirroredSpectrumBox(Color body, int position, int readout) {
         fill(body.R, body.G, body.B);
---
+++ src/no/demo/SceneButterflies.java
@@ -0,0 +1,76 @@
+package no.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import processing.core.PApplet;
+
+public class SceneButterflies implements Scene {
+
+	private List<Butterfly> butterflies;
+	private int x, y;
+	private boolean turn;
+	private int speed = 5;
+
+	private Butterfly GenerateRandomButterfly(PApplet parent) {
+        int Min = 0;
+        int Max = 250;
+
+        Color randColor1 = Color.random(Min, Max);
+        Color randColor2 = Color.random(Min, Max);
+
+        int PosMax = 2000;
+
+        int SpeedMax = 10;
+
+        int position = Min + (int) (Math.random() * ((PosMax - Min) + 1));
+
+        int speed = 1 + (int) (Math.random() * ((SpeedMax - 1) + 1));
+
+        return new Butterfly(parent, randColor1, randColor2, position, 0, speed);
+    }
+	
+	public SceneButterflies(PApplet parent) {
+		this.butterflies = new ArrayList<Butterfly>();
+		
+		for (int i = 0; i != 10; ++i) {
+			this.butterflies.add(GenerateRandomButterfly(parent));
+		}
+		
+		this.x = 0;
+		this.y = 0;
+	}
+
+	@Override
+	public void draw(PApplet render) {
+
+		if (turn) {
+			x -= speed;
+			y -= speed;
+		} else {
+			x += speed;
+			y += speed;
+		}
+
+		if (x > 200) {
+			turn = true;
+		}
+
+		if (x < 1) {
+			turn = false;
+		}
+
+		for (Butterfly butterfly : butterflies) {
+			butterfly.move();
+			butterfly.draw(x, y);
+		}
+
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
